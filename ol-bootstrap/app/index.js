335f064c2550d574f5365d77ce5645dbc421fbd5
MaxGuo <maxlovesbbq@gmail.com>
2023-04-04 09:46:55 -0400

    Update index.js

--- ol-bootstrap/app/index.js
+++ ol-bootstrap/app/index.js
@@ -54,8 +54,7 @@ document.getElementById("addFeatureDrawerContent").innerHTML = `<p>Add new Obser
                                                             <button id="addPointBtn">Add Observation</button>`;
 
 
-let geojsonObject = 
-{
+let hospitalJson = {
   "type": "FeatureCollection",
   "features": [
     {
@@ -63,109 +62,68 @@ let geojsonObject =
       "properties": {},
       "geometry": {
         "coordinates": [
-          [
-            -75.67166745894251,
-            45.42933043383789
-          ],
-          [
-            -75.6671199708729,
-            45.42135143326155
-          ],
-          [
-            -75.66274738619163,
-            45.41631794516795
-          ],
-          [
-            -75.66362190312806,
-            45.41447631302282
-          ],
-          [
-            -75.6707929420061,
-            45.41349408465473
-          ],
-          [
-            -75.672891782653,
-            45.409196634677784
-          ],
-          [
-            -75.66939371490814,
-            45.4050216558756
-          ],
-          [
-            -75.66921881152066,
-            45.40293405075988
-          ],
-          [
-            -75.66974352168221,
-            45.3993726639888
-          ],
-          [
-            -75.67394120297688,
-            45.394582858861185
-          ],
-          [
-            -75.67691456056109,
-            45.38966981646345
-          ],
-          [
-            -75.68286127572779,
-            45.38807298572496
-          ],
-          [
-            -75.68600953669942,
-            45.386230432663325
-          ],
-          [
-            -75.68600953669942,
-            45.385247713139364
-          ],
-          [
-            -75.68670915024839,
-            45.38365075747626
-          ],
-          [
-            -75.69003231460667,
-            45.38254514635989
-          ],
-          [
-            -75.69492960945033,
-            45.38242229934545
-          ],
-          [
-            -75.69982690429397,
-            45.37885961981456
-          ],
-          [
-            -75.69807787042112,
-            45.37087348813739
-          ],
-          [
-            -75.69230605864102,
-            45.36190310338807
-          ],
-          [
-            -75.69038212138075,
-            45.35846203041899
-          ],
-          [
-            -75.70245045510326,
-            45.354774934288116
-          ]
+          -75.66829356321553,
+          45.39596363499578
         ],
-        "type": "LineString"
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -75.6487879258227,
+          45.4014110115379
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -75.6359319375423,
+          45.40358981510846
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -75.72126906529505,
+          45.39253930637295
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -75.7308002290209,
+          45.38864776580928
+        ],
+        "type": "Point"
       }
     }
   ]
 }
   
 let jsonVectorSource = new ol.source.Vector({
-    features: new ol.format.GeoJSON().readFeatures(geojsonObject),
+    features: new ol.format.GeoJSON().readFeatures(hospitalJson),
+    projection: "EPSG:4326"
   });
   
-jsonVectorSource.addFeature(new ol.Feature(new ol.geom.Circle([5e6, 7e6], 1e6)));
+// jsonVectorSource.addFeature(new ol.Feature(new ol.geom.Circle([5e6, 7e6], 1e6)));
 
 
-const vectorLayer = new ol.layer.Vector({
+const jsonVectorLayer = new ol.layer.Vector({
 source: jsonVectorSource
 });
 
@@ -180,9 +138,8 @@ let drawVectorLayer = new ol.layer.Vector({source: drawSource});
 const map = new ol.Map({
     layers: [
         new ol.layer.Tile({ source: new ol.source.OSM()}),
-        ottawaTileLayer,
-        drawVectorLayer,
-        vectorLayer
+        // ottawaTileLayer,
+        drawVectorLayer
     ],
     target: 'map',
     view: new ol.View({
@@ -222,15 +179,29 @@ map.on('click', async (evt) => {
                                                                   <button id="enableEditButton">Enable Edit</button>
                                                                   <button id="disableEditButton" hidden>Disable Edit</button>
                                                                   <button id="updateFeatureBtn" hidden>Apply Edit</button>
+                                                                </div>
+                                                                <div>
+                                                                  <button id="findHospital">Find Close to Hospital</button>
                                                                 </div>`;
-      let updateFeature = ()=>{
+      document.getElementById("findHospital").addEventListener("click", ()=>{
+        let drawFeatures = drawSource.getFeatures();
+        let hospitalFeatures = jsonVectorSource.getFeatures();
+        let intersectFeatures = [];
+        hospitalFeatures.forEach(feature => {
+          if (mapFeatures[0].getGeometry().intersectsCoordinate(feature.getGeometry().getCoordinates())) {
+      
+            // add the intersecting feature to the array
+            intersectFeatures.push(feature);
+          }
+        });
+      });
+      document.getElementById("updateFeatureBtn").addEventListener("click", ()=>{
         mapFeatures[0].setProperties({
           name : document.getElementById("featureName").value,
           type : document.getElementById("featureType").value,
           orientation : document.getElementById("featureOrientation").value,
         })
-      }
-      document.getElementById("updateFeatureBtn").addEventListener("click", updateFeature);
+      });
       document.getElementById("enableEditButton").addEventListener("click", ()=>{
         document.getElementById("currentObservation").hidden = true;
         document.getElementById("enableEditButton").hidden = true;
@@ -247,24 +218,4 @@ map.on('click', async (evt) => {
       });
     }
 });
-
-// document.getElementById('zoom-out').onclick = function () {
-//     const view = map.getView();
-//     const zoom = view.getZoom();
-//     view.setZoom(zoom - 1);
-//     let line = turf.lineString([[-83, 30], [-84, 36], [-78, 41]]);
-//     let options = {units: 'miles'};
-
-//     let along = turf.along(line, 200, options);
-//     console.log(along)
-// };
-
-// document.getElementById('zoom-in').onclick = function () {
-// const view = map.getView();
-// const zoom = view.getZoom();
-// view.setZoom(zoom + 1);
-
-// let vectorSourceFeatures = vectorLayer.getSource().getFeatures();
-// let layerJson = new ol.format.GeoJSON().writeFeaturesObject(vectorSourceFeatures);
-// };
 
