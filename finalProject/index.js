432757fb2311e158e80f86e96f91dd2872eead46
MaxGuo <maxlovesbbq@gmail.com>
2023-04-04 08:39:40 -0400

    add docker scripts and sidebar in bootstrap

--- finalProject/index.js
+++ finalProject/index.js
@@ -1,3 +1,4 @@
+
 class OpenSideBarControl extends ol.control.Control {
   /**
    * @param {Object} [opt_options] Control options.
@@ -39,6 +40,7 @@ class OpenSideBarControl extends ol.control.Control {
 
     document.getElementById("addPointBtn").addEventListener("click", addPointDraw);
 
+
   }
 }
 
@@ -53,145 +55,30 @@ document.getElementById("addFeatureDrawerContent").innerHTML = `<p>Add new Obser
                                                             <label>Observation Media: </label><input type="file" id="newObervationMedia"><br><br>
                                                             <button id="addPointBtn">Add Observation</button>`;
 
-
-let geojsonObject = 
-{
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "coordinates": [
-          [
-            -75.67166745894251,
-            45.42933043383789
-          ],
-          [
-            -75.6671199708729,
-            45.42135143326155
-          ],
-          [
-            -75.66274738619163,
-            45.41631794516795
-          ],
-          [
-            -75.66362190312806,
-            45.41447631302282
-          ],
-          [
-            -75.6707929420061,
-            45.41349408465473
-          ],
-          [
-            -75.672891782653,
-            45.409196634677784
-          ],
-          [
-            -75.66939371490814,
-            45.4050216558756
-          ],
-          [
-            -75.66921881152066,
-            45.40293405075988
-          ],
-          [
-            -75.66974352168221,
-            45.3993726639888
-          ],
-          [
-            -75.67394120297688,
-            45.394582858861185
-          ],
-          [
-            -75.67691456056109,
-            45.38966981646345
-          ],
-          [
-            -75.68286127572779,
-            45.38807298572496
-          ],
-          [
-            -75.68600953669942,
-            45.386230432663325
-          ],
-          [
-            -75.68600953669942,
-            45.385247713139364
-          ],
-          [
-            -75.68670915024839,
-            45.38365075747626
-          ],
-          [
-            -75.69003231460667,
-            45.38254514635989
-          ],
-          [
-            -75.69492960945033,
-            45.38242229934545
-          ],
-          [
-            -75.69982690429397,
-            45.37885961981456
-          ],
-          [
-            -75.69807787042112,
-            45.37087348813739
-          ],
-          [
-            -75.69230605864102,
-            45.36190310338807
-          ],
-          [
-            -75.69038212138075,
-            45.35846203041899
-          ],
-          [
-            -75.70245045510326,
-            45.354774934288116
-          ]
-        ],
-        "type": "LineString"
-      }
-    }
-  ]
-}
-  
-let jsonVectorSource = new ol.source.Vector({
-    features: new ol.format.GeoJSON().readFeatures(geojsonObject),
-  });
-  
-jsonVectorSource.addFeature(new ol.Feature(new ol.geom.Circle([5e6, 7e6], 1e6)));
-
-
-const vectorLayer = new ol.layer.Vector({
-source: jsonVectorSource
-});
-
-const url = 'https://maps.ottawa.ca/arcgis/rest/services/Zoning/MapServer';
-
-let ottawaTileLayer = new ol.layer.Tile({source:new ol.source.TileArcGISRest({url: url})});
-
+                                                    
 // draw source
 let drawSource = new ol.source.Vector({wrapX: false});
 let drawVectorLayer = new ol.layer.Vector({source: drawSource});
 
+// the city of ottawa map server  
+const url = 'https://maps.ottawa.ca/arcgis/rest/services/Zoning/MapServer';
+
+let ottawaTileLayer = new ol.layer.Tile({source:new ol.source.TileArcGISRest({url: url})});
+
 const map = new ol.Map({
     layers: [
-        new ol.layer.Tile({ source: new ol.source.OSM()}),
+        new ol.layer.Tile({source: new ol.source.OSM()}),
         ottawaTileLayer,
-        drawVectorLayer,
-        vectorLayer
+        jsonVectorSource,
+        drawVectorLayer
     ],
     target: 'map',
     view: new ol.View({
-        center: [-75.6972, 45.4215],
-        zoom: 14,
-        projection: "EPSG:4326"
-    }),
+      projection : "EPSG:4326",
+      center: [-75.6972, 45.4215],
+      zoom: 10
+  }),
 });
-
 
 map.addControl(new OpenSideBarControl);
 const modify = new ol.interaction.Modify({source: drawSource});
@@ -247,24 +134,3 @@ map.on('click', async (evt) => {
       });
     }
 });
-
-// document.getElementById('zoom-out').onclick = function () {
-//     const view = map.getView();
-//     const zoom = view.getZoom();
-//     view.setZoom(zoom - 1);
-//     let line = turf.lineString([[-83, 30], [-84, 36], [-78, 41]]);
-//     let options = {units: 'miles'};
-
-//     let along = turf.along(line, 200, options);
-//     console.log(along)
-// };
-
-// document.getElementById('zoom-in').onclick = function () {
-// const view = map.getView();
-// const zoom = view.getZoom();
-// view.setZoom(zoom + 1);
-
-// let vectorSourceFeatures = vectorLayer.getSource().getFeatures();
-// let layerJson = new ol.format.GeoJSON().writeFeaturesObject(vectorSourceFeatures);
-// };
-
