a29061d7d6d257fc0189fa6cec58865c198d1baf
MaxGuo <maxlovesbbq@gmail.com>
2023-04-07 20:37:19 -0400

    add about page

--- ol-bootstrap/app/index.js
+++ ol-bootstrap/app/index.js
@@ -19,9 +19,9 @@ class OpenSideBarControl extends ol.control.Control {
     });
     document.getElementById("closeAddDrawer").addEventListener("click", ()=>{document.getElementById("addFeatureDrawer").style.width = "0";} );
 
-    let addPointDraw = ()=>{
+    document.getElementById("addObservationBtn").addEventListener("click", ()=>{
       let pointDraw = new ol.interaction.Draw({
-          source: drawSource,
+          source: observationSource,
           stopClick: true,
           type: "Point"
         });
@@ -33,17 +33,38 @@ class OpenSideBarControl extends ol.control.Control {
           type: document.getElementById("newFeatureType").value,
           mediaPath : document.getElementById("newObervationMedia").value,
         })
-      
       })
-    }
+    });
 
-    document.getElementById("addPointBtn").addEventListener("click", addPointDraw);
+    document.getElementById("findNumberOfObservations").addEventListener("click", ()=>{
+      let polygonDraw = new ol.interaction.Draw({
+          // source: drawSelectSource,
+          stopClick: true,
+          type: "Polygon"
+        });
+      map.addInteraction(polygonDraw);
+      polygonDraw.on('drawend', (evt) => {
+        let polygonRegionGeometry = evt.feature.getGeometry();
+        let observationFeatures = observationSource.getFeatures();
+        let selectedObservationFeatures = [];
+        observationFeatures.forEach((observationFeature)=>{
+          let observationFeatureGeometry = observationFeature.getGeometry();
+          if(polygonRegionGeometry.intersectsCoordinate(observationFeatureGeometry.getCoordinates())){
+            selectedObservationFeatures.push(observationFeature);
+          }
+        })
+        // drawSelectSource.removeFeature(evt.feature);
+        // drawSelectSource.refresh();
+        map.removeInteraction(polygonDraw);
+      })
+    });
 
   }
 }
 
-document.getElementById("addFeatureDrawerContent").innerHTML = `<p>Add new Observation:</p>
-                                                            <label>Observation Name: </label><input type="text" id="newFeatureName"><br>
+
+
+document.getElementById("addFeatureDrawerContent").innerHTML = `<label>Observation Name: </label><input type="text" id="newFeatureName"><br>
                                                             <label>Observation Type: </label><br>
                                                             <select id="newFeatureType">
                                                               <option value="UAS">UAS</option>
@@ -51,8 +72,11 @@ document.getElementById("addFeatureDrawerContent").innerHTML = `<p>Add new Obser
                                                               <option value="CUAS">CUAS</option>
                                                             </select><br>
                                                             <label>Observation Media: </label><input type="file" id="newObervationMedia"><br><br>
-                                                            <button id="addPointBtn">Add Observation</button>`;
+                                                            <button id="addObservationBtn">Add Observation</button>
+                                                            <button id="findNumberOfObservations">Find Number of observation within region</button>`;
 
+
+// hospital source and layer
 
 let hospitalJson = {
   "type": "FeatureCollection",
@@ -114,32 +138,129 @@ let hospitalJson = {
     }
   ]
 }
-  
-let jsonVectorSource = new ol.source.Vector({
-    features: new ol.format.GeoJSON().readFeatures(hospitalJson),
-    projection: "EPSG:4326"
-  });
-  
-// jsonVectorSource.addFeature(new ol.Feature(new ol.geom.Circle([5e6, 7e6], 1e6)));
 
-
-const jsonVectorLayer = new ol.layer.Vector({
-source: jsonVectorSource
+let hospitalJsonVectorSource = new ol.source.Vector({
+  features: new ol.format.GeoJSON().readFeatures(hospitalJson),
+  projection: "EPSG:4326"
 });
 
-const url = 'https://maps.ottawa.ca/arcgis/rest/services/Zoning/MapServer';
+const hospitalJsonVectorLayer = new ol.layer.Vector({
+  source: hospitalJsonVectorSource
+});
 
+
+
+// hardware store source and layer
+
+let hardwareStoreJson = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -75.60981607683134,
+          45.41726898875109
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -75.66071467015202,
+          45.364353967026545
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -75.7583210558081,
+          45.3525677605077
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -75.91726926431585,
+          45.302009357420445
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -75.71304692211976,
+          45.37453696716355
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -75.66788251408691,
+          45.28037085525892
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -75.6703148573255,
+          45.38166537346288
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
+let hardwareJsonVectorSource = new ol.source.Vector({
+  features: new ol.format.GeoJSON().readFeatures(hardwareStoreJson),
+  projection: "EPSG:4326"
+});
+
+const hardwareJsonVectorLayer = new ol.layer.Vector({
+  source: hardwareJsonVectorSource
+});
+
+// ottawa data source and layer
+const url = 'https://maps.ottawa.ca/arcgis/rest/services/Zoning/MapServer';
 let ottawaTileLayer = new ol.layer.Tile({source:new ol.source.TileArcGISRest({url: url})});
 
-// draw source
-let drawSource = new ol.source.Vector({wrapX: false});
-let drawVectorLayer = new ol.layer.Vector({source: drawSource});
+// observation source and layer
+let observationSource = new ol.source.Vector({wrapX: false});
+let observationLayer = new ol.layer.Vector({source: observationSource});
+
+// select source and layer
+let drawSelectSource = new ol.source.Vector({wrapX: false});
+let drawSelectLayer = new ol.layer.Vector({source: drawSelectSource});
 
 const map = new ol.Map({
     layers: [
         new ol.layer.Tile({ source: new ol.source.OSM()}),
         // ottawaTileLayer,
-        drawVectorLayer
+        observationLayer,
+        drawSelectLayer
     ],
     target: 'map',
     view: new ol.View({
@@ -151,14 +272,14 @@ const map = new ol.Map({
 
 
 map.addControl(new OpenSideBarControl);
-const modify = new ol.interaction.Modify({source: drawSource});
-map.addInteraction(modify);
-modify.on("modifyend", (evt)=>{
+const observationModify = new ol.interaction.Modify({source: observationSource});
+map.addInteraction(observationModify);
+observationModify.on("modifyend", (evt)=>{
   // some modify events
 })
 
 map.on('click', async (evt) => {
-    let mapFeatures = await drawVectorLayer.getFeatures(evt.pixel);
+    let mapFeatures = await observationLayer.getFeatures(evt.pixel);
     if (mapFeatures.length != 0) {
       document.getElementById("editDrawer").style.width = "250px";
       document.getElementById("closeEditDrawer").addEventListener("click", ()=>{document.getElementById("editDrawer").style.width = "0";} );
@@ -181,18 +302,33 @@ map.on('click', async (evt) => {
                                                                   <button id="updateFeatureBtn" hidden>Apply Edit</button>
                                                                 </div>
                                                                 <div>
-                                                                  <button id="findHospital">Find Close to Hospital</button>
+                                                                  <button id="findHospital">Find Hospital within 3 km</button>
+                                                                  <button id="findHardwareStore">Find Hardware Store within 3km</button>
                                                                 </div>`;
       document.getElementById("findHospital").addEventListener("click", ()=>{
-        let currentObservationBuffer = ol.extent.buffer(mapFeatures[0].getGeometry().getExtent(), 0.001); 
+        let currentObservationBuffer = ol.extent.buffer(mapFeatures[0].getGeometry().getExtent(), 0.03); 
         let observationBufferGeometry = new ol.geom.Polygon.fromExtent(currentObservationBuffer);
         let hospitalFeatures = jsonVectorSource.getFeatures();
-        let intersectFeatures = [];
+        let hospitalFeaturesWithin3km = [];
         hospitalFeatures.forEach(hospitalFeature => {
           if (observationBufferGeometry.intersectsCoordinate(hospitalFeature.getGeometry().getCoordinates())) {
       
             // add the intersecting feature to the array
-            intersectFeatures.push(hospitalFeature);
+            hospitalFeaturesWithin3km.push(hospitalFeature);
+          }
+        });
+      });
+
+      document.getElementById("findHardwareStore").addEventListener("click", ()=>{
+        let currentObservationBuffer = ol.extent.buffer(mapFeatures[0].getGeometry().getExtent(), 0.03); 
+        let observationBufferGeometry = new ol.geom.Polygon.fromExtent(currentObservationBuffer);
+        let hardwareStoreFeatures = jsonVectorSource.getFeatures();
+        let hardwareFeautresWithin3km = [];
+        hardwareStoreFeatures.forEach(hospitalFeature => {
+          if (observationBufferGeometry.intersectsCoordinate(hospitalFeature.getGeometry().getCoordinates())) {
+      
+            // add the intersecting feature to the array
+            hardwareFeautresWithin3km.push(hospitalFeature);
           }
         });
       });
